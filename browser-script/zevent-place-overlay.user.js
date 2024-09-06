f06d12913df3b65336b112ffdb308e16d8639b53
PiR1 <pirddeveloppeur@gmail.com>
2024-09-06 13:26:13 +0200

    feat: transform imgur url

--- browser-script/zevent-place-overlay.user.js
+++ browser-script/zevent-place-overlay.user.js
@@ -2,7 +2,7 @@
 // @name         zevent-place-overlay
 // @namespace    http://tampermonkey.net/
 // @license      MIT
-// @version      2.0.3
+// @version      2.1.0
 // @description  Please organize with other participants on Discord: https://discord.gg/sXe5aVW2jV ; Press H to hide/show again the overlay.
 // @author       ludolpif, ventston, PiRDub
 // @match        https://place.zevent.fr/
@@ -19,7 +19,7 @@
  */
 (function () {
     'use strict';
-    const version = "2.0.3";
+    const version = "2.1.0";
     const scriptUpdateURL = "https://github.com/Ventston/zevent-zplace-overlays/raw/main/browser-script/zevent-place-overlay.user.js"
     // Global constants and variables for our script
     const overlayJSON1 = "https://pixels-solidaires.fr/overlays.json"; // Need CORS header (Access-Control-Allow-Origin: https://place.zevent.fr)
@@ -610,7 +610,11 @@
     function urlSanityCheck(url) {
         if (!url) return null;
         if (typeof url !== "string") return '#nonstring';
-        const trimmedURL = url.substring(0, 260).replaceAll(/\s/g, '');
+        let trimmedURL = url.substring(0, 260).replaceAll(/\s/g, '');
+        if(trimmedURL.includes("imgur.com") && !trimmedURL.includes(".png")){
+            const imgurId = trimmedURL.split("/").pop();
+            trimmedURL = "https://i.imgur.com/" + imgurId + ".png";
+        }
         if (!trimmedURL.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)) {
             zpoLog("urlSanityCheck(url) invalid : " + url);
             return '#invalid';
