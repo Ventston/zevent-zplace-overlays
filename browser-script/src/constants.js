e0950c941b6b831be8a2182ac3fbd2712aae0d5e
PiR1 <pirddeveloppeur@gmail.com>
2026-07-23 23:10:18 +0200

    feat: update server base URL

--- browser-script/src/constants.js
+++ browser-script/src/constants.js
@@ -1,7 +1,7 @@
 export const version = GM_info.script.version;
 export const scriptUpdateURL = GM_info.script.updateURL;
 
-export const serverBase = 'https://zevent-pixels.eu';
+export const serverBase = 'https://api-zevent-place.4each.dev';
 export const overlaysJsonUrl = serverBase + '/overlays.json';
 export const versionJsonUrl = 'https://raw.githubusercontent.com/Ventston/zevent-zplace-overlays/main/browser-script/version.json';
 export const symbolsUrl = serverBase + '/symbols.json';
