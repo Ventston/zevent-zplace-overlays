3ff9d2ed8ba5844e5535e107d4d97698619d4602
PiR1 <pirddeveloppeur@gmail.com>
2026-07-26 17:54:53 +0200

    refactor: update versionJsonUrl to point to dev branch

--- browser-script/src/constants.js
+++ browser-script/src/constants.js
@@ -3,7 +3,7 @@ export const scriptUpdateURL = GM_info.script.updateURL;
 
 export const serverBase = 'https://api-zevent-place.4each.dev';
 export const overlaysJsonUrl = serverBase + '/overlays.json';
-export const versionJsonUrl = 'https://raw.githubusercontent.com/Ventston/zevent-zplace-overlays/main/browser-script/version.json';
+export const versionJsonUrl = 'https://raw.githubusercontent.com/Ventston/zevent-zplace-overlays/dev/browser-script/version.json';
 export const symbolsUrl = serverBase + '/symbols.json';
 
 export const analyticsUrl = 'https://stats.4each.dev/api/send';
