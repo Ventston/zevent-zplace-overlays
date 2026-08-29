c2ddf5ad19d8841834cbd24de3600ead6f08e973
PiR1 <pirddeveloppeur@gmail.com>
2026-08-29 20:24:27 +0200

    feat: update version to 4.2.1 and change version URL to main branch

--- browser-script/src/constants.js
+++ browser-script/src/constants.js
@@ -6,7 +6,7 @@ export const presenceAttribute = 'data-zpo-version';
 
 export const serverBase = 'https://api-zevent-place.4each.dev';
 export const overlaysJsonUrl = serverBase + '/overlays.json';
-export const versionJsonUrl = 'https://raw.githubusercontent.com/Ventston/zevent-zplace-overlays/dev/browser-script/version.json';
+export const versionJsonUrl = 'https://raw.githubusercontent.com/Ventston/zevent-zplace-overlays/main/browser-script/version.json';
 export const symbolsUrl = serverBase + '/symbols.json';
 export const messagesJsonUrl = serverBase + '/messages.json';
 
